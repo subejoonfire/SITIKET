1d7a03c9e0ed2a4a46de2a3196b3e44cc340cc59
Harlan <harlanmuradi@gmail.com>
2024-12-02 15:12:52 +0800

    admin url added

--- resources/views/partial/sidebarAdminrill.blade.php
+++ resources/views/partial/sidebarAdminrill.blade.php
@@ -4,20 +4,20 @@
     <div class="sidebar-background"></div>
     <div class="sidebar-wrapper scrollbar-inner">
         <div class="sidebar-content">
-           
+
             <ul class="nav">
                 <li class="nav-item {{ request()->routeIs('dashboard_admin') ? 'active' : '' }}">
-                    <a href="{{ url('dashboard') }}">
+                    <a href="{{ url('admin/dashboard') }}">
                         <i class="fas fa-home"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item {{ request()->routeIs('user') ? 'active' : '' }}">
-                        <a href="{{ url('user') }}">
+                    <a href="{{ url('admin/user') }}">
                         <i class="fas fa-user-alt"></i>
                         <p>User</p>
                     </a>
-                  
+
                 </li>
             </ul>
 
