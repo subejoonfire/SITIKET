bb76df928abf68055dc6cecbdbe8df4e1be9ab1d
Harlan <harlanmuradi@gmail.com>
2024-12-02 15:30:34 +0800

    data user show

--- app/Http/Controllers/Admin/ARoutesController.php
+++ app/Http/Controllers/Admin/ARoutesController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class ARoutesController extends Controller
 {
     public function index()
     {
         $data = [
-            'title' => 'SI-TIKET | DASHBOARD'
+            'title' => 'SI-TIKET | DASHBOARD',
         ];
         return view('pages.admin.dashboard', $data);
     }
-    
+
     public function user()
     {
         $data = [
-            'title' => 'SI-TIKET | USER'
+            'title' => 'SI-TIKET | USER',
+            'user' => User::all(),
+            // 'user' => User::whereNotNull('iddepartment')->get(),
         ];
         return view('pages.admin.user', $data);
     }
