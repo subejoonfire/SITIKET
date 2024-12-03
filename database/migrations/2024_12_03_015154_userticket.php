15e9ab7a3f585200a3505c7ae6c277a11aa488b2
Harlan <harlanmuradi@gmail.com>
2024-12-03 10:54:12 +0800

    tables update

--- database/migrations/2024_12_03_015154_userticket.php
+++ database/migrations/2024_12_03_015154_userticket.php
@@ -1,29 +1,29 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('userticket', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('iduser');
-            $table->unsignedBigInteger('idticket');
-            $table->timestamp('datetime');
-        });
-    }
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('usertickets', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('iduser');
+                $table->unsignedBigInteger('idticket');
+                $table->timestamp('datetime');
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('userticket');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('userticket');
+        }
+    };
--- database/migrations/2024_12_03_015200_userdepartment.php
+++ database/migrations/2024_12_03_015200_userdepartment.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('userdepartment', function (Blueprint $table) {
+        Schema::create('userdepartments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('iduser');
             $table->unsignedBigInteger('iddepartment');
