dcf758879029594b595cc26747cdac6aac5acd4a
FerdiNurrahman <ferdiart113@gmail.com>
2024-12-03 09:39:58 +0800

    status is active sidebar

--- resources/views/partial/sidebarAdminrill.blade.php
+++ resources/views/partial/sidebarAdminrill.blade.php
@@ -1,39 +1,34 @@
 <!-- Sidebar -->
 <div class="sidebar">
-
     <div class="sidebar-background"></div>
     <div class="sidebar-wrapper scrollbar-inner">
         <div class="sidebar-content">
-
             <ul class="nav">
-                <li class="nav-item {{ request()->routeIs('dashboard_admin') ? 'active' : '' }}">
-                    <a href="{{ url('admin/dashboard') }}">
+                <li class="nav-item {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+                    <a href="{{ route('admin.dashboard') }}">
                         <i class="fas fa-home"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
-                <li class="nav-item {{ request()->routeIs('user') ? 'active' : '' }}">
-                    <a href="{{ url('admin/user') }}">
+                <li class="nav-item {{ request()->routeIs('admin.user') ? 'active' : '' }}">
+                    <a href="{{ route('admin.user') }}">
                         <i class="fas fa-user-alt"></i>
                         <p>User</p>
                     </a>
-
                 </li>
-                <li class="nav-item {{ request()->routeIs('category') ? 'active' : '' }}">
-                    <a href="{{ url('admin/category') }}">
+                <li class="nav-item {{ request()->routeIs('admin.category') ? 'active' : '' }}">
+                    <a href="{{ route('admin.category') }}">
                         <i class="fas fa-box"></i>
                         <p>Category</p>
                     </a>
                 </li>
-                <li class="nav-item {{ request()->routeIs('department') ? 'active' : '' }}">
-                    <a href="{{ url('admin/depart') }}">
+                <li class="nav-item {{ request()->routeIs('admin.depart') ? 'active' : '' }}">
+                    <a href="{{ route('admin.depart') }}">
                         <i class="fas fa-building"></i>
                         <p>Department</p>
                     </a>
-
                 </li>
             </ul>
-
         </div>
     </div>
 </div>
