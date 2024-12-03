b029c5fe0d86fbd01333fc9aa924294f2a6244ef
Harlan <harlanmuradi@gmail.com>
2024-12-03 10:55:17 +0800

    functions update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Department;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
     /**
      * Store a new user.
      */
-    public function store(Request $request)
+    public function userStore(Request $request)
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
@@ -29,13 +30,13 @@ class AdminController extends Controller
         return redirect()->to(url('admin/user'))->with('success', 'User berhasil ditambahkan!');
     }
 
-    public function delete($id)
+    public function userDelete($id)
     {
         User::destroy($id);
         return redirect()->back()->with('success', 'User berhasil dihapus!');
     }
 
-    public function update(Request $request)
+    public function userUpdate(Request $request)
     {
         $validated = $request->validate([
             'id' => 'required|exists:users,id',
@@ -44,7 +45,6 @@ class AdminController extends Controller
             'email' => 'required|email|unique:users,email,' . $request->id,
             'password' => 'nullable|min:8|confirmed',
         ]);
-
         $user = User::findOrFail($validated['id']);
         $user->name = $validated['name'];
         $user->iddepartment = $validated['iddepartment'];
@@ -55,5 +55,39 @@ class AdminController extends Controller
         $user->save();
 
         return redirect()->back()->with('success', 'User berhasil diperbarui!');
+    }
+    public function departmentStore(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Department::create([
+            'departmentname' => $request->name,
+        ]);
+
+        return redirect()->to(url('admin/department'))->with('success', 'Departemen berhasil ditambahkan.');
+    }
+
+    public function departmentDelete($id)
+    {
+        $department = Department::findOrFail($id);
+        $department->delete();
+
+        return redirect()->back()->with('success', 'Departemen berhasil dihapus.');
+    }
+
+    public function departmentUpdate(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $department = Department::findOrFail($id);
+        $department->update([
+            'departmentname' => $request->name,
+        ]);
+
+        return redirect()->back()->with('success', 'Departemen berhasil diperbarui.');
     }
 }
