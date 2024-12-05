5d3e751b493771ef04c5140a54f8c39c000616bc
Harlan <harlanmuradi@gmail.com>
2024-12-05 14:49:51 +0800

    table relationship fixes

--- app/Models/UserTicket.php
+++ app/Models/UserTicket.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserTicket extends Model
 {
     protected $fillable = ['iduser', 'idticket', 'datetime'];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'iduser', 'id');
+    }
+    public function tickets(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class, 'idticket', 'id');
+    }
 }
