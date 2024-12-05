6ca37670d8372d2c40b29978a610401a6bf4ecae
Harlan <harlanmuradi@gmail.com>
2024-12-05 11:57:23 +0800

    update query to model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use App\Models\Category;
 use App\Models\Department;
+use App\Models\UserTicket;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -25,7 +27,18 @@ class Ticket extends Model
             ->join('users', 'users.id', '=', 'user_tickets.iduser')
             ->where('iduser', auth()->user()->id);
     }
-
+    public function tickets_helpdesk()
+    {
+        return $this->belongsToMany(Ticket::class, 'user_tickets', 'idticket', 'id')
+            ->leftJoin('departments', 'departments.id', '=', 'tickets.iddepartment')
+            ->join('users', 'users.id', '=', 'user_tickets.iduser')
+            ->select(
+                'user_tickets.*',
+                'users.*',
+                'tickets.*',
+                'departments.*',
+            );
+    }
     public function department()
     {
         return $this->belongsTo(Department::class, 'iddepartment');
