3df0b22f4f54db71218fc3a3be853d4cee3594a0
subejoonfire <harlanmuradi@gmail.com>
2024-12-11 16:15:57 +0800

    adding idmodule field

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Module extends Model
@@ -11,4 +13,9 @@ class Module extends Model
     protected $fillable = [
         'modulename',
     ];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'iduser');
+    }
 }
