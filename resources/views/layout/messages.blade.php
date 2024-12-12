14d9bf15fb2dd292b3a52fb9aa5a3b2d6c303462
Cryestly <katakai2004@gmail.com>
2024-12-12 10:45:30 +0800

    CHAT

--- resources/views/layout/messages.blade.php
+++ resources/views/layout/messages.blade.php
@@ -1,67 +1,235 @@
-@if ($data->status != 'DIAJUKAN' && $data->status != 'TERKIRIM')
-<h4 class="page-title">Pesan Masuk</h4>
-<div class="container">
-    <div class="panel messages-panel">
-        <div class="tab-content">
-            <div class="tab-pane message-body active" id="inbox-message-1">
-                <div class="new-message-wrapper">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Send message to...">
-                        <a class="btn btn-danger close-new-message" href="#"><i class="fa fa-times"></i></a>
-                    </div>
-                    <div class="chat-footer new-message-textarea">
-                        <textarea class="send-message-text"></textarea>
-                        <label class="upload-file">
-                            <input type="file" required="">
-                            <i class="fa fa-paperclip"></i>
-                        </label>
-                        <button type="button" class="send-message-button btn-info"> <i class="fa fa-send"></i> </button>
-                    </div>
+{{-- <link href="https://netdna.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet"> --}}
+   
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
+{{-- <div class="container bootdey"> --}}
+<div class="email-app mb-4">
+    <main class="inbox">
+        <div class="toolbar">
+            <div class="btn-group">
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-envelope"></span>
+                </button>
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-star"></span>
+                </button>
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-star-o"></span>
+                </button>
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-bookmark-o"></span>
+                </button>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-mail-reply"></span>
+                </button>
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-mail-reply-all"></span>
+                </button>
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-mail-forward"></span>
+                </button>
+            </div>
+            <button type="button" class="btn btn-light">
+                <span class="fa fa-trash-o"></span>
+            </button>
+            <div class="btn-group">
+                <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+                    <span class="fa fa-tags"></span>
+                    <span class="caret"></span>
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">add label <span class="badge badge-danger"> Home</span></a>
+                    <a class="dropdown-item" href="#">add label <span class="badge badge-info"> Job</span></a>
+                    <a class="dropdown-item" href="#">add label <span class="badge badge-success"> Clients</span></a>
+                    <a class="dropdown-item" href="#">add label <span class="badge badge-warning"> News</span></a>
                 </div>
             </div>
-            <div class="message-chat">
-                <div class="chat-body">
-                    <div class="message info">
-                        <img alt="" class="img-circle medium-image" src="https://bootdey.com/img/Content/avatar/avatar1.png">
-                        <div class="message-body">
-                            <div class="message-info">
-                                <h4> {{ $data->users->name }} </h4>
-                                <h5> <i class="fa fa-clock-o"></i> 2:25 PM </h5>
-                            </div>
-                            <hr>
-                            <div class="message-text">
-                                {{ $data->detailissue }}
-                            </div>
-                        </div>
-                        <br>
-                    </div>
-                    <div class="message my-message">
-                        <img alt="" class="img-circle medium-image" src="https://bootdey.com/img/Content/avatar/avatar1.png">
-                        <div class="message-body">
-                            <div class="message-body-inner">
-                                <div class="message-info">
-                                    <h4> Dennis Novac </h4>
-                                    <h5> <i class="fa fa-clock-o"></i> 2:28 PM </h5>
-                                </div>
-                                <hr>
-                                <div class="message-text">
-                                    Thanks, I think I will use this for my next dashboard system.
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-                    </div>
-                </div>
-                <div class="chat-footer">
-                    <textarea class="send-message-text"></textarea>
-                    <label class="upload-file">
-                        <input type="file" required="">
-                        <i class="fa fa-paperclip"></i>
-                    </label>
-                    <button type="button" class="send-message-button btn-info"> <i class="fas fa-paper-plane"></i> </button>
-                </div>
+            <div class="btn-group float-right">
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-chevron-left"></span>
+                </button>
+                <button type="button" class="btn btn-light">
+                    <span class="fa fa-chevron-right"></span>
+                </button>
             </div>
         </div>
-    </div>
+
+        <ul class="messages">
+            <li class="message unread">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">
+                        <span class="fa fa-paper-clip"></span> Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">
+                        <span class="fa fa-paper-clip"></span> Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message unread">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+
+                </a>
+            </li>
+            <li class="message">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message unread">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+            <li class="message">
+                <a href="#">
+                    <div class="actions">
+                        <span class="action"><i class="fa fa-square-o"></i></span>
+                        <span class="action"><i class="fa fa-star-o"></i></span>
+                    </div>
+                    <div class="header">
+                        <span class="from">Lukasz Holeczek</span>
+                        <span class="date">Today, 3:47 PM</span>
+                    </div>
+                    <div class="title">
+                        Lorem ipsum dolor sit amet.
+                    </div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </main>
 </div>
-@endif
+</div>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+	
+</script>
+</body>
+</html>
