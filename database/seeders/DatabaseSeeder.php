cb3a0e632d8995c1423d7bf57415bc06ccd4ab7e
subejoonfire <harlanmuradi@gmail.com>
2024-12-13 16:26:01 +0800

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -18,58 +19,89 @@ class DatabaseSeeder extends Seeder
         DB::table('departments')->truncate();
 
         // Seed users table
+        $password = Hash::make('Jhonlin@123');
+
         DB::table('users')->insert([
             [
+                'id' => 1,
+                'idmodule' => null,
                 'name' => 'admin',
+                'image' => null,
                 'phone' => '084728188291',
-                'level' => 1,
+                'level' => '1',
                 'email' => 'admin@example.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('Jhonlin@123'),
+                'password' => $password,
                 'remember_token' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
+                'id' => 2,
+                'idmodule' => null,
                 'name' => 'Ferdi Nurrahman',
+                'image' => null,
                 'phone' => '084728188291',
-                'level' => 1,
+                'level' => '1',
                 'email' => 'ferdinurrahman@gmail.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('Jhonlin@123'),
+                'password' => $password,
                 'remember_token' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
+                'id' => 3,
+                'idmodule' => null,
                 'name' => 'helpdesk',
+                'image' => null,
                 'phone' => '084728188291',
-                'level' => 2,
+                'level' => '2',
                 'email' => 'helpdesk@gmail.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('Jhonlin@123'),
+                'password' => $password,
                 'remember_token' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
+                'id' => 4,
+                'idmodule' => null,
                 'name' => 'Ferdi',
+                'image' => null,
                 'phone' => '083142170067',
-                'level' => 3,
+                'level' => '3',
                 'email' => 'ferdiart113@gmail.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('Jhonlin@123'),
+                'password' => $password,
                 'remember_token' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'name' => 'user',
+                'id' => 5,
+                'idmodule' => null,
+                'name' => 'Andre',
+                'image' => null,
                 'phone' => '084728188291',
-                'level' => 4,
+                'level' => '4',
                 'email' => 'user@gmail.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('Jhonlin@123'),
+                'password' => $password,
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 6,
+                'idmodule' => '1',
+                'name' => 'RAUL',
+                'image' => null,
+                'phone' => '1232423',
+                'level' => '3',
+                'email' => 'PIC1@gmail.com',
+                'email_verified_at' => null,
+                'password' => $password,
                 'remember_token' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -105,7 +137,23 @@ class DatabaseSeeder extends Seeder
             'SCFUEL',
             'MPLANT'
         ];
-
+        DB::table('tickets')->insert([
+            [
+                'id' => 1,
+                'iddepartment' => null,
+                'idmodule' => 1,
+                'iduser' => 5,
+                'iduser_pic' => 6,
+                'idcategory' => 1,
+                'ticketcode' => 'TKT5202412130',
+                'status' => 'DIAJUKAN',
+                'issue' => 'dsaf',
+                'detailissue' => 'adsf',
+                'priority' => 'Bisa Menunggu',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
         foreach ($departments as $department) {
             DB::table('departments')->insert([
                 'departmentname' => $department,
@@ -113,5 +161,21 @@ class DatabaseSeeder extends Seeder
                 'updated_at' => now(),
             ]);
         }
+        DB::table('modules')->insert([
+            [
+                'id' => 1,
+                'modulename' => 'TANK',
+                'created_at' => Carbon::parse('2024-12-13 07:48:29'),
+                'updated_at' => Carbon::parse('2024-12-13 07:48:29'),
+            ],
+        ]);
+        DB::table('categories')->insert([
+            [
+                'id' => 1,
+                'categoryname' => 'SOFTWARE',
+                'created_at' => Carbon::parse('2024-12-13 07:49:34'),
+                'updated_at' => Carbon::parse('2024-12-13 07:49:34'),
+            ],
+        ]);
     }
 }
