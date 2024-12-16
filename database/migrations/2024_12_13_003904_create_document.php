a55082fb7d0338fa8d28ec7de67b084a618265d0
subejoonfire <harlanmuradi@gmail.com>
2024-12-16 15:11:10 +0800

    adding new atribute for unique filename

--- database/migrations/2024_12_13_003904_create_document.php
+++ database/migrations/2024_12_13_003904_create_document.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('idmessage')->nullable()->default(NULL);
             $table->string('documentname');
+            $table->string('path_documentname')->unique();
             $table->timestamps();
             $table->softDeletes();
         });
