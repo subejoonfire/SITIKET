f1d6868bf22b444a8fb6dfcb69f34392c8629276
KuronoKushito <fansyou25@gmail.com>
2024-12-17 09:03:00 +0800

    User notification

--- resources/views/layout/mainuser.blade.php
+++ resources/views/layout/mainuser.blade.php
@@ -381,15 +381,78 @@ background:#eee
 
             <!-- Navbar Header -->
             <nav class="navbar navbar-header navbar-expand-lg">
-                <div class="container-fluid">
-                    <div class="collapse" id="search-nav"></div>
-                    <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
-                        {{-- <li class="nav-item toggle-nav-search hidden-caret">
-                            <a class="nav-link" data-toggle="collapse" href="#search-nav" role="button" aria-expanded="false" aria-controls="search-nav">
-                                <i class="fa fa-search"></i>
-                            </a>
-                        </li> --}}
+				
+				<div class="container-fluid">
+					<div class="collapse" id="search-nav">
+					</div>
+					<ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
+						<li class="nav-item dropdown hidden-caret">
+							<a class="nav-link dropdown-toggle" href="#" id="notifDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<i class="fa fa-bell"></i>
+								<span class="notification">3</span>
+							</a>
+							<ul class="dropdown-menu notif-box animated fadeIn" aria-labelledby="notifDropdown">
+								<li>
+									<div class="dropdown-title">You have 4 new notification</div>
+								</li>
+								<li>
+									<div class="notif-scroll scrollbar-outer">
+										<div class="notif-center">
+											<a href="#">
+												<div class="notif-img"> 
+													<img src="../assets/img/profile2.jpg" alt="Img Profile">
+												</div>
+												<div class="notif-content">
+													<span class="block">
+														Rizky send messages to you
+													</span>
+													<span class="time">12 minutes ago</span> 
+												</div>
+											</a>
+											<a href="#">
+												<div class="notif-img"> 
+													<img src="../assets/img/profile2.jpg" alt="Img Profile">
+												</div>
+												<div class="notif-content">
+													<span class="block">
+														Reza send messages to you
+													</span>
+													<span class="time">12 minutes ago</span> 
+												</div>
+											</a>
+                                            <a href="#">
+												<div class="notif-img"> 
+													<img src="../assets/img/profile2.jpg" alt="Img Profile">
+												</div>
+												<div class="notif-content">
+													<span class="block">
+														Ferdi send messages to you
+													</span>
+													<span class="time">12 minutes ago</span> 
+												</div>
+											</a>
+                                            <a href="#">
+												<div class="notif-img"> 
+													<img src="../assets/img/profile2.jpg" alt="Img Profile">
+												</div>
+												<div class="notif-content">
+													<span class="block">
+														Reza send messages to you
+													</span>
+													<span class="time">12 minutes ago</span> 
+												</div>
+											</a>
+
+										</div>
+									</div>
+								</li>
+								<li>
+									<a class="see-all" href="javascript:void(0);">See all notifications<i class="fa fa-angle-right"></i> </a>
+								</li>
+							</ul>
+						</li>
                         <li class="nav-item dropdown hidden-caret">
+                            
                             <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
                                 <div class="avatar-sm">
                                     <img src="{{ url('back-end/assets/img/' . (auth()->user()->image ?? 'default.jpg')) }}" alt="..." class="avatar-img rounded-circle">
--- resources/views/partial/sidebar/user.blade.php
+++ resources/views/partial/sidebar/user.blade.php
@@ -4,10 +4,12 @@
     <div class="sidebar-wrapper scrollbar-inner">
         <div class="sidebar-content">
             <ul class="nav">
+                
                 <li class="nav-item {{ request()->routeIs('user/dashboard') ? 'active' : '' }}">
                     <a href="{{ url('user') }}">
                         <i class="fas fa-home"></i>
                         <p>Beranda</p>
+                        <span class="badge badge-count">5</span>
                     </a>
                 </li>
 
@@ -16,3 +18,15 @@
     </div>
 </div>
 <!-- End Sidebar -->
+<style>
+    .badge-count {
+        background-color: #ff6161; /* Ubah warna menjadi merah */
+        color: white; /* Warna teks putih */
+        font-size: 12px; /* Ukuran teks */
+        border-radius: 50%; /* Membuat lingkaran */
+        padding: 3px 7px; /* Padding untuk ukuran badge */
+        line-height: 1;
+        display: inline-block; /* Menjaga agar badge tetap proporsional */
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.2); /* Efek bayangan */
+    }
+</style>
