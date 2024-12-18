51a141462f610baea230bce1e294d4d25fffaad2
subejoonfire <harlanmuradi@gmail.com>
2024-12-18 10:48:49 +0800

    companies companycode added

--- database/migrations/2024_12_17_013557_create_companies.php
+++ database/migrations/2024_12_17_013557_create_companies.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('companyname');
+            $table->string('companycode');
             $table->timestamps();
             $table->softDeletes();
         });
