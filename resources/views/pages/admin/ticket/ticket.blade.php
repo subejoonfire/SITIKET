46b2019e14e9e728fcfd0ef188fa1fb2e0389afb
subejoonfire <harlanmuradi@gmail.com>
2024-12-18 10:49:23 +0800

    file name changed

---
+++ resources/views/pages/admin/ticket/ticket.blade.php
@@ -0,0 +1,116 @@
+@extends('layout.mainadmin')
+@section('content')
+
+<div class="main-panel">
+    <div class="content">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">Tiket</h4>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex align-items-center">
+                                <h4 class="card-title">Semua Tiket</h4>
+                                {{-- <button class="btn btn-primary btn-round ml-auto" data-toggle="modal" data-target="#addRowModal">
+                                    <i class="fa fa-plus"></i>
+                                   
+                                </button> --}}
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <!-- Modal -->
+                            <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header no-bd">
+                                            <h5 class="modal-title">
+                                                <span class="fw-mediumbold">
+                                                    New</span>
+                                                <span class="fw-light">
+                                                    Row
+                                                </span>
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p class="small">Create a new row using this form, make sure you fill them all</p>
+                                            <form>
+                                                <div class="row">
+                                                    <div class="col-sm-12">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Name</label>
+                                                            <input id="addName" type="text" class="form-control" placeholder="fill name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6 pr-0">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Position</label>
+                                                            <input id="addPosition" type="text" class="form-control" placeholder="fill position">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Office</label>
+                                                            <input id="addOffice" type="text" class="form-control" placeholder="fill office">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer no-bd">
+                                            <button type="button" id="addRowButton" class="btn btn-primary">Add</button>
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-responsive">
+                                <table id="add-row" class="display table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Kode Tiket</th>
+                                            <th>Nama Pelapor</th>
+                                            <th>Module</th>
+                                            <th>Status</th>
+                                            <th>Masalah</th>
+                                            <th>Tanggal Diajukan</th>
+                                            <th style="width: 10%" data-orderable="false">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($collection as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->ticketcode }}</td>
+                                            <td>{{ $item->users->name }}</td>
+                                            <td>{{ $item->modules->modulename }}</td>
+                                            <td>{{ $item->status }}</td>
+                                            <td>{{ $item->issue }}</td>
+                                            <td>{{ $item->created_at->format('l, d F Y H:i') }}</td>
+                                            <td>
+                                                <div class="btn-review">
+                                                    <a href="{{ url('admin/ticket/review/' . $item->id)}}" class="btn btn-info btn-sm">
+                                                        <i class="fa fa-eye"></i> Review
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endsection
