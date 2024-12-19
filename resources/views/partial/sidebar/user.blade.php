87723278a1824042e1d5f2396643bc27239ba9de
subejoonfire <harlanmuradi@gmail.com>
2024-12-19 09:36:54 +0800

    notification update

--- resources/views/partial/sidebar/user.blade.php
+++ resources/views/partial/sidebar/user.blade.php
@@ -5,15 +5,16 @@
     <div class="sidebar-wrapper scrollbar-inner">
         <div class="sidebar-content">
             <ul class="nav">
-                
+
                 <li class="nav-item {{ request()->routeIs('user/dashboard') ? 'active' : '' }}">
                     <a href="{{ url('user') }}">
                         <i class="fas fa-home"></i>
                         <p>Beranda</p>
-                        <span class="badge badge-count">5</span>
+                        @if ($notification != 0)
+                        <span class="badge badge-count">{{ $notification }}</span>
+                        @endif
                     </a>
                 </li>
-
             </ul>
         </div>
     </div>
