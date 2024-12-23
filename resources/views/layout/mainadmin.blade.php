15a358605f0ca4c227a39beb0e5a9849b25e6630
subejoonfire <harlanmuradi@gmail.com>
2024-12-23 16:52:48 +0800

    admin view update

--- resources/views/layout/mainadmin.blade.php
+++ resources/views/layout/mainadmin.blade.php
@@ -4,10 +4,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>{{ $title ?? 'SI-TIKET' }}</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-    <link rel="icon" type="image/jpeg" href="{{ asset('JG.png') }}">
+    <link rel="icon" type="image/jpeg" href="JG.png">
 
     <!-- Fonts and icons -->
-    <script src="{{ url('back-end/assets/js/plugin/webfont/webfont.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/webfont/webfont.min.js"></script>
     <script>
         WebFont.load({
             google: {
@@ -15,7 +15,7 @@
             }
             , custom: {
                 "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"]
-                , "urls": ["{{ url('back-end/assets/css/fonts.css') }}"]
+                , "urls": ["back-end/assets/css/fonts.css"]
             }
             , active: function() {
                 sessionStorage.fonts = true;
@@ -25,10 +25,10 @@
     </script>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="{{ url('back-end/assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('back-end/assets/css/azzara.min.css') }}">
-    <link rel="stylesheet" href="{{ url('back-end/assets/css/demo.css') }}">
-    <link rel="stylesheet" href="{{ url('back-end/assets/css/pic.css') }}">
+    <link rel="stylesheet" href="back-end/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="back-end/assets/css/azzara.min.css">
+    <link rel="stylesheet" href="back-end/assets/css/demo.css">
+    <link rel="stylesheet" href="back-end/assets/css/pic.css">
     <style>
         .main-header {
             background-color: #70c55b !important;
@@ -67,8 +67,6 @@
         }
 
     </style>
-
-
 </head>
 <body>
     <div class="wrapper">
@@ -99,21 +97,16 @@
                 <div class="container-fluid">
                     <div class="collapse" id="search-nav"></div>
                     <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
-                        {{-- <li class="nav-item toggle-nav-search hidden-caret">
-                            <a class="nav-link" data-toggle="collapse" href="#search-nav" role="button" aria-expanded="false" aria-controls="search-nav">
-                                <i class="fa fa-search"></i>
-                            </a>
-                        </li> --}}
                         <li class="nav-item dropdown hidden-caret">
                             <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
                                 <div class="avatar-sm">
-                                    <img src="{{ url('storage/profiles/' . (auth()->user()->image ?? 'default.jpg')) }}" alt="..." class="avatar-img rounded-circle">
+                                    <img src="storage/profiles/{{ auth()->user()->image ?? 'default.jpg' }}" alt="..." class="avatar-img rounded-circle">
                                 </div>
                             </a>
                             <ul class="dropdown-menu dropdown-user animated fadeIn">
                                 <li>
                                     <div class="user-box">
-                                        <div class="avatar-lg"><img src="{{ url('storage/profiles/' . (auth()->user()->image ?? 'default.jpg')) }}" alt="image profile" class="avatar-img rounded"></div>
+                                        <div class="avatar-lg"><img src="storage/profiles/{{ auth()->user()->image ?? 'default.jpg' }}" alt="image profile" class="avatar-img rounded"></div>
                                         <div class="u-text">
                                             <h4>{{ auth()->user()->name }}</h4>
                                             <p class="text-muted">{{ auth()->user()->email}}</p>
@@ -122,9 +115,9 @@
                                 </li>
                                 <li>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="{{ url('profile') }}">My Profile</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="{{ url('logout') }}">Logout</a>
+                                    <a class="dropdown-item" href="profile">My Profile</a>
+                                    <div class="dropdown-divider </div>
+                                    <a class=" dropdown-item" href="logout">Logout</a>
                                 </li>
                             </ul>
                         </li>
@@ -141,54 +134,54 @@
     </div>
 
     <!-- JS Files -->
-    <script src="{{ url('back-end/assets/js/core/jquery.3.2.1.min.js') }}"></script>
-    <script src="{{ url('back-end/assets/js/core/popper.min.js') }}"></script>
-    <script src="{{ url('back-end/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="back-end/assets/js/core/jquery.3.2.1.min.js"></script>
+    <script src="back-end/assets/js/core/popper.min.js"></script>
+    <script src="back-end/assets/js/core/bootstrap.min.js"></script>
 
     <!-- jQuery UI -->
-    <script src="{{ url('back-end/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
-    <script src="{{ url('back-end/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+    <script src="back-end/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
 
     <!-- jQuery Scrollbar -->
-    <script src="{{ url('back-end/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
 
     <!-- Moment JS -->
-    <script src="{{ url('back-end/assets/js/plugin/moment/moment.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/moment/moment.min.js"></script>
 
     <!-- Chart JS -->
-    <script src="{{ url('back-end/assets/js/plugin/chart.js/chart.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/chart.js/chart.min.js"></script>
 
     <!-- jQuery Sparkline -->
-    <script src="{{ url('back-end/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
 
     <!-- Chart Circle -->
-    <script src="{{ url('back-end/assets/js/plugin/chart-circle/circles.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/chart-circle/circles.min.js"></script>
 
     <!-- Datatables -->
-    <script src="{{ url('back-end/assets/js/plugin/datatables/datatables.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/datatables/datatables.min.js"></script>
 
     <!-- Bootstrap Notify -->
-    <script src="{{ url('back-end/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
 
     <!-- Bootstrap Toggle -->
-    <script src="{{ url('back-end/assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js"></script>
 
     <!-- jQuery Vector Maps -->
-    <script src="{{ url('back-end/assets/js/plugin/jqvmap/jquery.vmap.min.js') }}"></script>
-    <script src="{{ url('back-end/assets/js/plugin/jqvmap/maps/jquery.vmap.world.js') }}"></script>
+    <script src="back-end/assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
+    <script src="back-end/assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
 
     <!-- Google Maps Plugin -->
-    <script src="{{ url('back-end/assets/js/plugin/gmaps/gmaps.js') }}"></script>
+    <script src="back-end/assets/js/plugin/gmaps/gmaps.js"></script>
 
     <!-- Sweet Alert -->
-    <script src="{{ url('back-end/assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
+    <script src="back-end/assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
     <!-- Azzara JS -->
-    <script src="{{ url('back-end/assets/js/ready.min.js') }}"></script>
+    <script src="back-end/assets/js/ready.min.js"></script>
 
     <!-- Azzara DEMO methods -->
-    <script src="{{ url('back-end/assets/js/setting-demo.js') }}"></script>
-    <script src="{{ url('back-end/assets/js/demo.js') }}"></script>
+    <script src="back-end/assets/js/setting-demo.js"></script>
+    <script src="back-end/assets/js/demo.js"></script>
 
     <script>
         $(document).ready(function() {
@@ -239,7 +232,7 @@
                 }
             });
 
-            item.addEventListener('mouseleave', () => {
+            item.addEventListener('mouseleave ', () => {
                 const dropdown = item.querySelector('.dropdown-menu');
                 if (dropdown) {
                     dropdown.style.opacity = '0';
