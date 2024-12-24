8c873947fd36b187acb21b99b696462a11cc6190
subejoonfire <harlanmuradi@gmail.com>
2024-12-24 10:32:39 +0800

    code optimization

--- resources/views/layout/mainLanding.blade.php
+++ resources/views/layout/mainLanding.blade.php
@@ -7,28 +7,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-
-    <!-- Favicon -->
     <link rel="icon" type="image/jpeg" href="JG.png">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="templates/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="templates/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="templates/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="templates/css/style.css" rel="stylesheet">
 
 </head>
 
