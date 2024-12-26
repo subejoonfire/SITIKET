7ea4f26a86126c5b92ac3e3d38c741b7cdb01427
subejoonfire <harlanmuradi@gmail.com>
2024-12-26 14:29:49 +0800

    code optimization

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,3 +1,4 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <title>{{ $title ?? 'SI-TIKET' }}</title>
 <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
