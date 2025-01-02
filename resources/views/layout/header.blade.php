a015bc47ff0a752ffa34c6853c99f2809b786a38
subejoonfire <harlanmuradi@gmail.com>
2025-01-02 13:56:18 +0800

    header update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -17,7 +17,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-<script src="{{ asset('back-end/assets/js/plugin/webfont/webfont.min.js') }}"></script>
+<script src="{{ asset('back-end/assets/js/plugin/webfon t/webfont.min.js') }}"></script>
 <script>
     WebFont.load({
         google: {
