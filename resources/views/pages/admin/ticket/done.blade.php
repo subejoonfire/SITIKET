8506cf36fdc9749dea8fe749ee80f782edb52d00
FerdiNurrahman <ferdiart113@gmail.com>
2025-01-16 10:59:52 +0800

    perubahan nama

--- resources/views/pages/admin/ticket/done.blade.php
+++ resources/views/pages/admin/ticket/done.blade.php
@@ -12,7 +12,7 @@
                     <div class="card">
                         <div class="card-header">
                             <div class="d-flex align-items-center">
-                                <h4 class="card-title">Semua Tiket</h4>
+                                <h4 class="card-title">Tiket Selesai</h4>
                                 {{-- <button class="btn btn-primary btn-round ml-auto" data-toggle="modal" data-target="#addRowModal">
                                     <i class="fa fa-plus"></i>
                                    
